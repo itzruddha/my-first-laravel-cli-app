b5c509fad99ac7de88eb40cfb1a0230b52ad1ac0
itzruddha <aniruddhamandal540@gmail.com>
2025-01-01 12:05:00 +0530

    add users

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/user/{id}', function ($id) {
+//     $user = App\Models\User::find($id);
+//     return $user;
+// });
+
+
+Route::resource('users', UserController::class);
